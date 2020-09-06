5a0392400a32432cd7e52bb09dcbd903bad4a9d8
Richard Kurwitz <kurwitz3@aol.com>
2020-09-06 23:26:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,18 +20,18 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
-    return_hash = {}
+    hash = {}
 
       social = doc.css(".vitals-container .social-icon-container a")
-      social.each do |element| #iterate through each of the social elements and assign the keys if the item exists
-        if element.attr('href').include?("twitter")
-          return_hash[:twitter] = element.attr('href')
-        elsif element.attr('href').include?("linkedin")
-          return_hash[:linkedin] = element.attr('href')
-        elsif element.attr('href').include?("github")
-          return_hash[:github] = element.attr('href')
-        elsif element.attr('href').end_with?("com/")
-          return_hash[:blog] = element.attr('href')
+      social.each do |x| 
+        if x.attr('href').include?("twitter")
+          hash[:twitter] = x.attr('href')
+        elsif x.attr('href').include?("linkedin")
+          hash[:linkedin] = x.attr('href')
+        elsif x.attr('href').include?("github")
+          hash[:github] = x.attr('href')
+        elsif x.attr('href').end_with?("com/")
+          hash[:blog] = x.attr('href')
         end
       end
       return_hash[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text

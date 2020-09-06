db783862a4bc3b4e107618d2eca5176eb903cbd3
Richard Kurwitz <kurwitz3@aol.com>
2020-09-06 23:28:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,8 +22,8 @@ class Scraper
     doc = Nokogiri::HTML(html)
     hash = {}
 
-      social = doc.css(".vitals-container .social-icon-container a")
-      social.each do |x| 
+      srcape = doc.css(".vitals-container .social-icon-container a")
+      scrape.each do |x| 
         if x.attr('href').include?("twitter")
           hash[:twitter] = x.attr('href')
         elsif x.attr('href').include?("linkedin")
@@ -34,30 +34,12 @@ class Scraper
           hash[:blog] = x.attr('href')
         end
       end
-      return_hash[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
-      return_hash[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
-
-  return_hash
+      hash[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
+      hash[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
+hash
   end
 
 end
     
  
 
-
-  #kickstarter.css("li.project.grid_4").each do |project|
-  #title = project.css("h2.bbcard_name strong a").text
-  #projects[title.to_sym] =  projects[title.to_sym] = {
-      #:image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-      #:description => project.css("p.bbcard_blurb").text,
-      #:location => project.css("ul.project-meta span.location-name").text,
-      #:percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-   # }
-  #end
- 
-  # return the projects hash
-  #projects
-#end
-#x.css('a').attr('href').value   student url 
-#x.css('p').children[0].text      location
-#x.css('h4').children[0].text      name 
